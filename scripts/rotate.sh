3abc2ff78a2093506c72d42d20d19a08815335a2
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2015-09-10 20:46:16 +0200

    thanks gvacca for the bug report!

--- scripts/rotate.sh
+++ scripts/rotate.sh
@@ -30,14 +30,18 @@ $programm homer_statistic stats_useragent 0 20
 # Dealing with calls, registrations and a rest of captured data
 # delete tables older then 30 days
 TS=`/usr/bin/date +%s`;
+MYSQL_USER="sipcapture"
+MYSQL_PASSWORD="sipcapturestrongpassword"
+MYSQL_DB="homer_data"
+
 let TS=TS-2592000;
-for table in `/usr/bin/mysql -D homer_data -B -e "show tables" | /usr/bin/grep sip_capture`;
+for table in `/usr/bin/mysql -u $MYSQL_USER -p $MYSQL_PASSWORD -D $MYSQL_DB -B -e "show tables" | /usr/bin/grep sip_capture`;
 do
 	table_date=`/usr/bin/echo $table | /usr/bin/awk -F'_' '{print $4}'`;
 	table_ts=`/usr/bin/date --date=$table_date +%s`;
 	if [ $table_ts -lt $TS ];
 	then
 		/usr/bin/logger "sipcapture: Deleting table $table";
-		/usr/bin/mysql -D homer_data -e "drop table $table";
+		/usr/bin/mysql -u $MYSQL_USER -p $MYSQL_PASSWORD -D $MYSQL_DB -e "drop table $table";
 	fi;
 done;
