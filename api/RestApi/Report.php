073a92e7a19f7a3a074fb989355ed9709f6aa996
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2016-03-16 13:45:50 +0100

    Update Report.php

--- api/RestApi/Report.php
+++ api/RestApi/Report.php
@@ -165,7 +165,7 @@ class Report {
                  
             $db->dbconnect_node($node);
             $limit = $limit_orig;
-            if(empty($callwhere)) $callwhere = generateWhere($search, $and_or, $db, 0);
+            $callwhere = generateWhere($search, $and_or, $db, 0);
             $table = "rtcp_capture";
             $query = "SELECT *, '".$node['name']."' as dbnode FROM ".$table." WHERE (`date` BETWEEN FROM_UNIXTIME(".$time['from_ts'].") AND FROM_UNIXTIME(".$time['to_ts']."))";
             if(count($callwhere)) $query .= " AND ( " .implode(" AND ", $callwhere). ")";
