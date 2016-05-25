e6d02c09005a0180e1b7641dec495a96ea8fead2
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2016-05-25 13:51:23 +0200

    added webrtc table

--- sql/schema_data.sql
+++ sql/schema_data.sql
@@ -64,6 +64,38 @@ CREATE TABLE IF NOT EXISTS `report_capture` (
 
 -- --------------------------------------------------------
 
+
+--
+-- Table structure for table `report_capture`
+--
+
+CREATE TABLE IF NOT EXISTS `webrtc_capture` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `micro_ts` bigint(18) NOT NULL DEFAULT '0',
+  `correlation_id` varchar(256) NOT NULL DEFAULT '',
+  `source_ip` varchar(60) NOT NULL DEFAULT '',
+  `source_port` int(10) NOT NULL DEFAULT 0,
+  `destination_ip` varchar(60) NOT NULL DEFAULT '',
+  `destination_port` int(10) NOT NULL DEFAULT 0,
+  `proto` int(5) NOT NULL DEFAULT 0,
+  `family` int(1) DEFAULT NULL,
+  `type` int(5) NOT NULL DEFAULT 0,
+  `node` varchar(125) NOT NULL DEFAULT '',
+  `msg` varchar(1500) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`,`date`),
+  KEY `date` (`date`),
+  KEY `correlationid` (`correlation_id`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8
+/*!50100 PARTITION BY RANGE ( UNIX_TIMESTAMP(`date`))
+(PARTITION p2013082901 VALUES LESS THAN (1377734400) ENGINE = InnoDB,
+ PARTITION p2013090821 VALUES LESS THAN (1378670400) ENGINE = InnoDB,
+ PARTITION p2013090822 VALUES LESS THAN (1378674000) ENGINE = InnoDB,
+ PARTITION p2013090823 VALUES LESS THAN (1378677600) ENGINE = InnoDB,
+ PARTITION pmax VALUES LESS THAN MAXVALUE ENGINE = InnoDB) */;
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `rtcp_capture`
 --
