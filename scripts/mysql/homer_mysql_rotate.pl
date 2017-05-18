ece4cda9e391037f822c444c42117e573636c4ff
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2017-05-18 10:38:21 +0200

    fixed rotation script in mysql directory. Removed the old one

--- scripts/mysql/homer_mysql_rotate.pl
+++ scripts/mysql/homer_mysql_rotate.pl
@@ -187,7 +187,6 @@ CREATE TABLE IF NOT EXISTS `[TRANSACTION]_[TIMESTAMP]` (
 PARTITION pmax VALUES LESS THAN MAXVALUE ENGINE = InnoDB) */ ;
 END
 
-
 my $WEBRTC_DATA_TABLE=<<END;
 CREATE TABLE IF NOT EXISTS `[TRANSACTION]_[TIMESTAMP]` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -217,6 +216,29 @@ CREATE TABLE IF NOT EXISTS `[TRANSACTION]_[TIMESTAMP]` (
 PARTITION pmax VALUES LESS THAN MAXVALUE ENGINE = InnoDB) */ ;
 END
 
+my $RTCP_DATA_TABLE=<<END;
+CREATE TABLE IF NOT EXISTS `[TRANSACTION]_[TIMESTAMP]` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `micro_ts` bigint(18) NOT NULL DEFAULT '0',
+  `correlation_id` varchar(256) NOT NULL DEFAULT '',
+  `source_ip` varchar(60) NOT NULL DEFAULT '',
+  `source_port` int(10) NOT NULL DEFAULT 0,
+  `destination_ip` varchar(60) NOT NULL DEFAULT '',
+  `destination_port` int(10) NOT NULL DEFAULT 0,
+  `proto` int(5) NOT NULL DEFAULT 0,
+  `family` int(1) DEFAULT NULL,
+  `type` int(5) NOT NULL DEFAULT 0,
+  `node` varchar(125) NOT NULL DEFAULT '',
+  `msg` varchar([MSG_SIZE]) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`,`date`),
+  KEY `date` (`date`),
+  KEY `correlationid` (`correlation_id`(255))
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPRESSED KEY_BLOCK_SIZE=8 COMMENT='[TIMESTAMP]'
+/*!50100 PARTITION BY RANGE ( UNIX_TIMESTAMP(`date`))
+([PARTITIONS]
+PARTITION pmax VALUES LESS THAN MAXVALUE ENGINE = InnoDB) */ ;
+END
 
 #Check DATA tables
 my $db = db_connect($CONFIG, "db_data");
@@ -236,17 +258,20 @@ foreach my $table (keys %{ $CONFIG->{"DATA_TABLE_ROTATION"} }) {
     #SIP Data tables
     my $is_isup = $table=~/^isup_/;
     my $is_webrtc = $table=~/^webrtc_/;
-    if($table=~/^sip_/ || $is_isup || $is_webrtc) {
+    my $is_rtcp = $table=~/^rtcp_/;
+    if($table=~/^sip_/ || $is_isup || $is_webrtc || $is_rtcp) {
         my $curtstamp;
         for(my $y=0; $y<($newtables+1); $y++) {
 	    my $data_table = $ORIGINAL_DATA_TABLE;
 	    if($is_isup) {
 		$data_table =  $ISUP_DATA_TABLE;
 	    }
-	    elsif($is_isup) {
+	    elsif($is_webrtc) {
 		$data_table =  $WEBRTC_DATA_TABLE;
 	    }
-	
+            elsif($is_rtcp) {
+                $data_table =  $RTCP_DATA_TABLE;
+            }
             $curtstamp = time()+(86400*$y);
             new_data_table($curtstamp, $mystep, $partstep, $data_table, $table);
         }
@@ -263,6 +288,9 @@ foreach my $table (keys %{ $CONFIG->{"DATA_TABLE_ROTATION"} }) {
         while(my @ref = $sth->fetchrow_array()) {
            my $table_name = $ref[0];
            my($proto, $cap, $type, $ts) = split(/_/, $table_name, 4);
+	   # RTCP has only 3 underscores 
+	   $ts = $type if($table eq "rtcp_capture");
+
            $ts+=0;
            if($ts < $oldest) {
                say "Removing table: $table_name" if($CONFIG->{"SYSTEM"}{"debug"} == 1);
